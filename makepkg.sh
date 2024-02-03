8e1ff2f63a7d530bf121bcc84ddb76ff721a58d8
SerraPi <95191556+SerraPi@users.noreply.github.com>
2024-02-03 07:15:44 -0700

    Update makepkg.sh

--- makepkg.sh
+++ makepkg.sh
@@ -12,7 +12,7 @@ sed_commands=()
 main() {
     check_sudo
     calculate_num_threads
-    display_message "Checking $makepkg_conf" "Makepkg.conf"
+    display_message "Checking $makepkg_conf"
 
     if [ -s "$makepkg_conf" ]; then
         if is_makeflags_commented; then
@@ -28,15 +28,15 @@ main() {
         execute_sed_commands
 
         if [ "$verbose_mode" = true ]; then
-            display_message "${GREEN}Makepkg configuration updated successfully!${NC}" "Makepkg.conf"
+            display_message "${GREEN}Makepkg configuration updated successfully!${NC}"
         fi
     else
-        display_message "Error: $makepkg_conf not found or is empty." "Makepkg.conf"
+        display_message "Error: $makepkg_conf not found or is empty."
     fi
 }
 
 check_sudo() {
-    sudo -v || (display_message "Error: This script requires sudo privileges. Exiting." "Makepkg.conf" && exit 1)
+    sudo -v || (display_message "Error: This script requires sudo privileges. Exiting." && exit 1)
 }
 
 calculate_num_threads() {
@@ -50,30 +50,11 @@ is_makeflags_commented() {
 
 is_j_value_present() {
     if sudo grep -qE '^\s*(#)?MAKEFLAGS\s*=\s*.*-j[0-9]+' "$makepkg_conf" && ! sudo grep -qE '^\s*(#)?MAKEFLAGS\s*=\s*""' "$makepkg_conf"; then
-        display_message "Detected -j value in MAKEFLAGS of $makepkg_conf" "Makepkg.conf"
+        display_message "Detected -j value in MAKEFLAGS of $makepkg_conf"
         return 0
     fi
-    display_message "No -j value detected in $makepkg_conf" "Makepkg.conf"
+    display_message "No -j value detected in $makepkg_conf"
     return 1
-}
-
-uncomment_makeflags() {
-    add_sed_command "sudo sed -i -e '/^\s*#.*MAKEFLAGS/s/^#//' \"$makepkg_conf\""
-    display_message "Uncommenting MAKEFLAGS in $makepkg_conf" "Makepkg.conf"
-}
-
-update_j_value() {
-    add_sed_command "sudo sed -i -e \"/^\s*MAKEFLAGS/s/-j[0-9]*/-j$num_threads/\" \"$makepkg_conf\""
-    display_message "Changing -j value in $makepkg_conf to -j$num_threads" "Makepkg.conf"
-}
-
-add_j_flag_if_missing() {
-    if sudo grep -qE '^\s*MAKEFLAGS\s*=\s*".*"' "$makepkg_conf"; then
-        add_sed_command "sudo sed -i -e 's/^\s*MAKEFLAGS\s*=\s*\"\(.*\)\"/MAKEFLAGS=\"\1 -j$num_threads\"/' \"$makepkg_conf\""
-    elif sudo grep -qE '^\s*MAKEFLAGS\s*=""' "$makepkg_conf" || sudo grep -qE '^\s*MAKEFLAGS\s*=\s*""' "$makepkg_conf"; then
-        add_sed_command "sudo sed -i -e 's/^\s*MAKEFLAGS\s*=\s*\"\(.*\)\"/MAKEFLAGS=\"-j$num_threads\"/' \"$makepkg_conf\""
-    fi
-    display_message "Adding -j$num_threads to $makepkg_conf (MAKEFLAGS)" "Makepkg.conf"
 }
 
 add_sed_command() {
@@ -86,10 +67,29 @@ execute_sed_commands() {
     done
 }
 
+uncomment_makeflags() {
+    add_sed_command "sudo sed -i -e '/^\s*#.*MAKEFLAGS/s/^#//' \"$makepkg_conf\""
+    display_message "Uncommenting MAKEFLAGS in $makepkg_conf"
+}
+
+update_j_value() {
+    add_sed_command "sudo sed -i -e \"/^\s*MAKEFLAGS/s/-j[0-9]*/-j$num_threads/\" \"$makepkg_conf\""
+    display_message "Changing -j value in $makepkg_conf to -j$num_threads"
+}
+
+add_j_flag_if_missing() {
+    if sudo grep -qE '^\s*MAKEFLAGS\s*=\s*".*"' "$makepkg_conf"; then
+        add_sed_command "sudo sed -i -e 's/^\s*MAKEFLAGS\s*=\s*\"\(.*\)\"/MAKEFLAGS=\"\1 -j$num_threads\"/' \"$makepkg_conf\""
+    elif sudo grep -qE '^\s*MAKEFLAGS\s*=""' "$makepkg_conf" || sudo grep -qE '^\s*MAKEFLAGS\s*=\s*""' "$makepkg_conf"; then
+        add_sed_command "sudo sed -i -e 's/^\s*MAKEFLAGS\s*=\s*\"\(.*\)\"/MAKEFLAGS=\"-j$num_threads\"/' \"$makepkg_conf\""
+    fi
+    display_message "Adding -j$num_threads to $makepkg_conf (MAKEFLAGS)"
+}
+
 display_message() {
     [ "$verbose_mode" = false ] && return
     local message="$1"
-    local file_name="${YELLOW}$2${NC}"
+    local file_name="${YELLOW}$(basename "$makepkg_conf")${NC}"
     echo -e "[$file_name] $message"
 }
 
