b197fe6d2d9ce795191cb68b5fb6b51b962553e4
SerraPi <95191556+SerraPi@users.noreply.github.com>
2024-02-03 15:15:40 -0700

    Add files via upload

---
+++ rate-mirrors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use yay to install rank-mirrors
+yay -S rank-mirrors --noconfirm
+
+TMPFILE="$(mktemp)"
+sudo true
+rate-mirrors --save="$TMPFILE" arch --max-delay=43200
+sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
+sudo mv "$TMPFILE" /etc/pacman.d/mirrorlist
